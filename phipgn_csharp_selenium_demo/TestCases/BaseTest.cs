164ef6c5336ef77be55513ebe58dadfa4cccd068
Phi <phipgn@gmail.com>
2020-09-08 16:01:35 +0700

    first commit

---
+++ phipgn_csharp_selenium_demo/TestCases/BaseTest.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Configuration;
+
+namespace phipgn_csharp_selenium_demo.TestCases
+{
+    class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        protected void setUp()
+        {
+            driver = new ChromeDriver();
+            Console.WriteLine("url=" + ConfigurationManager.AppSettings["URL"]);
+            GoToURL(ConfigurationManager.AppSettings["URL"]);
+            //GoToURL("https://www.amazon.com");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        protected void tearDown()
+        {
+            driver.Quit();
+        }
+
+        protected void WaitForElementPresent()
+        {
+
+        }
+
+        protected void WaitForElementDisplayed(IWebElement e)
+        {
+            int retry = 0;
+            do
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Element is displayed : {e.Displayed} : {retry}");
+                retry++;
+            } while (!e.Displayed && retry < 3);
+        }
+
+        protected void WaitForElementClickable(IWebElement e)
+        {
+
+        }
+
+        protected void ClickToElement(IWebElement e)
+        {
+            e.Click();
+        }
+
+        protected void GoToURL(String url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        protected void SelectByText(IWebElement e, string text)
+        {
+            var selectElement = new SelectElement(e);
+            selectElement.SelectByText(text);
+        }
+
+        protected void SetText(IWebElement e, string text)
+        {
+            e.SendKeys(text);
+        }
+    }
+}
---
+++ phipgn_csharp_selenium_demo/TestCases/HomePageTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using phipgn_csharp_selenium_demo.PageObjects;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Threading;
+
+namespace phipgn_csharp_selenium_demo.TestCases
+{
+    class HomePageTest : BaseTest
+    {
+        [Test]
+        public void Test01()
+        {
+            var homePage = new HomePage(driver);
+            /*
+            SelectByText(homePage.searchDropdownBox, "Books");
+            SetText(homePage.searchBox, "Selenium");
+            ClickToElement(homePage.btnSearch);*/
+            homePage.SearchBook("Selenium");
+            Thread.Sleep(5000);
+        }
+    }
+}
